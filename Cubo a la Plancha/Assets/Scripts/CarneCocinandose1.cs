3301977da433e7e0d7a44258f6302d93be6b2c53
RichardPDLR <rapuertasd@unadvirtual.edu.co>
2024-04-03 22:36:41 -0500

    La carne ya cocina y nuevos modelos
    Se cambio el modelo de la Sarten a uno mucho mejor, el Script CarneCocinada1 ahora si cambia el color de las caras cuando estan en contacto con la sarten con la ayuda de un nuevo Script llamado DetectarObjeto, tambien se agrego un modelo de mario como referencia pero aun no tiene animaciones.

--- Cubo a la Plancha/Assets/Scripts/CarneCocinandose1.cs
+++ Cubo a la Plancha/Assets/Scripts/CarneCocinandose1.cs
@@ -4,59 +4,25 @@ using UnityEngine;
 
 public class CarneCocinandose1 : MonoBehaviour
 {
-    public Color colorAlContacto = Color.red; // Color al contacto con otro objeto
-    private Color colorOriginal; // Color original del objeto
-    private Renderer rend; // Referencia al componente Renderer
-    private bool enContacto = false; // Estado de contacto
-    private bool cambioColorActivado = false; // Estado de cambio de color activado
-    public float tiempoAntesDeCambio = 1.0f; // Tiempo antes de cambiar de color
+    public Color colorAlContacto = Color.red;
+    private Color colorOriginal;
+    private Renderer rend;
+    public bool enContacto = false;
+    
 
     void Start()
     {
-        // Obtener el componente Renderer del objeto
         rend = GetComponent<Renderer>();
-
-        // Guardar el color original del objeto
         colorOriginal = rend.material.color;
     }
 
-    void Update()
+    public void CambiarColor()
     {
-        // Verificar si el objeto está en contacto y el tiempo de espera ha pasado
-        if (enContacto && tiempoAntesDeCambio > 0)
-        {
-            tiempoAntesDeCambio -= Time.deltaTime;
-        }
-        else if (enContacto && !cambioColorActivado)
-        {
-            // Cambiar el color del objeto al color de contacto
-            rend.material.color = colorAlContacto;
-            cambioColorActivado = true;
-        }
+        rend.material.color = colorAlContacto;
     }
 
-    // Método que se ejecuta cuando otro objeto entra en contacto con el objeto actual
-    void OnTriggerEnter(Collider other)
+    public void RestaurarColor()
     {
-        // Verificar si el objeto que entró en contacto tiene un BoxCollider
-        if (other.GetComponent<BoxCollider>() != null)
-        {
-            // Activar el estado de contacto
-            enContacto = true;
-        }
-    }
-
-    // Método que se ejecuta cuando otro objeto deja de estar en contacto con el objeto actual
-    void OnTriggerExit(Collider other)
-    {
-        // Verificar si el objeto que dejó de estar en contacto tiene un BoxCollider
-        if (other.GetComponent<BoxCollider>() != null)
-        {
-            // Desactivar el estado de contacto
-            enContacto = false;
-
-            // Reiniciar el tiempo antes del cambio
-            tiempoAntesDeCambio = 1.0f;
-        }
-    }
+        rend.material.color = colorOriginal;
+    } 
 }
