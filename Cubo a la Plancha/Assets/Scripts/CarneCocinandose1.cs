2c692977eacc74dd335a7e96c316ec517552f2d6
RichardPDLR <rapuertasd@unadvirtual.edu.co>
2024-04-06 21:50:58 -0500

    Sonido de Carne Cocinandose
    Ahora la carne tendra un sonido de freir cuando se este cocinando una de las caras y cuando termine de cocinarse sonara un sonido que avisara que ya esta cocinada esa cara, tambien la carne ahora demora un poco mas cocinarse a diferencia de antes que se cocinaba enseguida

--- Cubo a la Plancha/Assets/Scripts/CarneCocinandose1.cs
+++ Cubo a la Plancha/Assets/Scripts/CarneCocinandose1.cs
@@ -6,23 +6,58 @@ public class CarneCocinandose1 : MonoBehaviour
 {
     public Color colorAlContacto = Color.red;
     private Color colorOriginal;
-    private Renderer rend;
-    public bool enContacto = false;
-    
+    private Renderer rend;    
+    public bool cocinado = false;
+    public float tiempoDeCoccion = 3f; // Tiempo en segundos para la cocción
+    public AudioSource sonidoCocinando; // Referencia al componente AudioSource
+    public AudioSource CarneCocinada; // Referencia al componente AudioSource
+    private bool sonidoReproducido = false; // Variable para controlar la reproducción del sonido
 
     void Start()
     {
         rend = GetComponent<Renderer>();
-        colorOriginal = rend.material.color;
-    }
+        colorOriginal = rend.material.color;        
+    }    
 
     public void CambiarColor()
-    {
-        rend.material.color = colorAlContacto;
-    }
+    {       
+        /// Si la carne no ha sido cocinada todavía
+        if (!cocinado)
+        {
+            /// Programar la restauración del color original después del tiempo de cocción
+            Invoke("RestaurarColor", tiempoDeCoccion);
+            
+            // Cambiar el color solo después de que haya pasado el tiempo de cocción
+            Invoke("CambiarColorInternamente", tiempoDeCoccion);
+
+            // Iniciar la reproducción del sonido
+            sonidoCocinando.Play();
+            sonidoReproducido = true;
+        }     
+    }    
 
     public void RestaurarColor()
     {
-        rend.material.color = colorOriginal;
-    } 
+        rend.material.color = colorOriginal;        
+        cocinado = false; // Reiniciar el estado de cocción
+
+        // Detener la reproducción del sonido
+        sonidoCocinando.Stop();
+
+        if(sonidoReproducido)
+        {
+            CarneCocinada.Play();
+            sonidoReproducido = false;
+        }
+    }
+
+    private void CambiarColorInternamente()
+    {
+        // Cambiar el color solo si aún no ha sido cocinado
+        if (!cocinado)
+        {
+            rend.material.color = colorAlContacto;
+            cocinado = true;
+        }
+    }
 }
